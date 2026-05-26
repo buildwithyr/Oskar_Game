2a33b3e459e87bebaa98286daaa9a2620c0a5237
buildwithyr <yannick.reiter@hotmail.com>
2026-05-26 10:09:24 +0200

    Add showLevelComplete function to utils

--- game/utils.js
+++ game/utils.js
@@ -6,3 +6,5 @@ function showScreen(id){
   document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"))
   document.getElementById(id).classList.add("active")
 }
+
+function showLevelComplete({ title, text, button, next, stars = 3 }){
